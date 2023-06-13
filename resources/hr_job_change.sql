79f12467ef4e6911842b43c6dc038351ca9553c8
skullfort <gumingyao@gmail.com>
2023-06-13 10:17:28 -0400

    Modify notebook to read data from PostgresSQL and add a screenshot of the database when all data is queried

--- resources/hr_job_change.sql
+++ resources/hr_job_change.sql
@@ -5,20 +5,20 @@
 -- Physical data model
 
 CREATE TABLE enrollee_data (
-    enrollee_id int   NOT NULL,
-    city varchar(255)   NOT NULL,
-    city_development_index float   NOT NULL,
-    gender varchar(255)   NOT NULL,
-    relevent_experience varchar(255)   NOT NULL,
-    enrolled_university varchar(255)   NOT NULL,
-    education_level varchar(255)   NOT NULL,
-    major_discipline varchar(255)   NOT NULL,
-    experience varchar(255)   NOT NULL,
-    company_size varchar(255)   NOT NULL,
-    company_type varchar(255)   NOT NULL,
-    last_new_job varchar(255)   NOT NULL,
-    training_hours int   NOT NULL,
-    target float   NOT NULL,
+    enrollee_id int,
+    city varchar(255),
+    city_development_index float,
+    gender varchar(255),
+    relevent_experience varchar(255),
+    enrolled_university varchar(255),
+    education_level varchar(255),
+    major_discipline varchar(255),
+    experience varchar(255),
+    company_size varchar(255),
+    company_type varchar(255),
+    last_new_job varchar(255),
+    training_hours int,
+    target float,
     CONSTRAINT pk_enrollee_data PRIMARY KEY (
         enrollee_id
      )
